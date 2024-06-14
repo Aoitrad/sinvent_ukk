2f4217c0f06e0d11e26814f442bc1ef5ddd88c42
Aoitrad <dzaky.ahmad8414@gmail.com>
2024-06-14 15:49:04 +0700

    tidak bisa dihapus jika  di tabel lain

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Validation\Rule;
+use Illuminate\Database\QueryException;
 
 class KategoriController extends Controller
 {
