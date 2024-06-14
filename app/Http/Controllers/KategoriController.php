dd4e0f2f2057c3b25609169fd49820997b91e5bf
Aoitrad <dzaky.ahmad8414@gmail.com>
2024-06-14 13:55:08 +0700

    whoa APIII

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -176,4 +176,60 @@ class KategoriController extends Controller
         $kategori = DB::select('SELECT ketKategori(?) AS description', [$kat]);
         return $kategori[0]->description ?? 'Unknown';
     }
+
+    // function untuk membuat index api
+    function showAPIKategori(Request $request){
+        $kategori = Kategori::all();
+        return response()->json($kategori);
+    }
+
+    function showAPIKategoriById($kategori_id){
+    $kategori = Kategori::find($kategori_id);
+
+    if (null == $kategori){
+        return response()->json(['status'=>"kategori tidak ditemukan"], 404);
+    }
+
+    return response()->json($kategori);
+    }
+
+    // function untuk create api
+    function createAPIKategori(Request $request){
+        $request->validate([
+            'deskripsi' => 'required|string|max:100',
+            'kategori' => 'required|in:M,A,BHP,BTHP',
+        ]);
+
+        // Simpan data kategori
+        $kat = Kategori::create([
+            'deskripsi' => $request->deskripsi,
+            'kategori' => $request->kategori,
+        ]);
+
+        return response()->json(["status"=>"data berhasil dibuat"]);
+    }
+
+    function updateAPIKategori(Request $request, $kategori_id){
+        $kategori = Kategori::find($kategori_id);
+
+        if (null == $kategori){
+            return response()->json(['status'=>"kategori tidak ditemukan"]);
+        }
+
+         $kategori->deskripsi= $request->deskripsi;
+         $kategori->kategori = $request->kategori;
+         $kategori->save();
+
+        return response()->json(["status"=>"kategori berhasil diubah"]);
+    }
+
+
+    // function untuk delete api
+    function deleteAPIKategori($kategori_id){
+
+        $del_kategori = Kategori::findOrFail($kategori_id);
+        $del_kategori -> delete();
+
+        return response()->json(["status"=>"data berhasil dihapus"]);
+    }
 }
